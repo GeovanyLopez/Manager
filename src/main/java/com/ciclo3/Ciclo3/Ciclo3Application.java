a669f783db1e4df7fbdcabaac2e797949d015c4e
Luis Geovany López <geovanyaprendecursos@gmail.com>
2022-08-30 20:38:58 -0500

    Hola Mundo!

--- src/main/java/com/ciclo3/Ciclo3/Ciclo3Application.java
+++ src/main/java/com/ciclo3/Ciclo3/Ciclo3Application.java
@@ -2,11 +2,18 @@ package com.ciclo3.Ciclo3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-
+@SpringBootApplication (exclude = {SecurityAutoConfiguration.class})
+@RestController
 public class Ciclo3Application {
 
+	@GetMapping("/hello")
+	public String inicio(){
+		return "Saldremos vivos de esta...";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Ciclo3Application.class, args);
 		System.out.println("Probando");
