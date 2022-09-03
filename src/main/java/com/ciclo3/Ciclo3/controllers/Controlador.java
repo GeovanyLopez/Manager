7691e2b9233fa016f402068d97380912832cfb2f
Luis Geovany López <geovanyaprendecursos@gmail.com>
2022-09-03 09:29:11 -0500

    Se creo template agregarEmpresa.html y los sercicios agregar y guardar empresa

--- src/main/java/com/ciclo3/Ciclo3/controllers/Controlador.java
+++ src/main/java/com/ciclo3/Ciclo3/controllers/Controlador.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -16,11 +18,27 @@ public class Controlador {
     @Autowired
     EmpresaServicios empresaServicios;
 
-    @GetMapping({"/Empresas", "/VerEmpresas"})
+    @GetMapping({"/", "/VerEmpresas"})
     public String viewEmpresas(Model model) {
         List<Empresa> listaEmpresas = empresaServicios.listarEmpresas();
         model.addAttribute("empresaList", listaEmpresas);
         return "verEmpresas";// verEmpresas es un archivo html
+    }
+
+    @GetMapping("/AgregarEmpresa")
+    public String nuevaEmpresa(Model model){
+        Empresa emp = new Empresa();
+        model.addAttribute("emp",emp);
+        return "agregarEmpresa";
+
+    }
+
+    @PostMapping("/GuardarEmpresa")
+    public String guardarEmpresa(Empresa emp, RedirectAttributes redirectAttributes){
+        if(empresaServicios.saveUpdate(emp) == true){
+            return "redirect:/VerEmpresas";// Se redirecciona al servicio
+        }
+        return "redirect:/AgregarEmpresa";
     }
 
 }
